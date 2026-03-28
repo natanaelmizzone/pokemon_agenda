b70e2e640c2ee76a562e0835de257ee6d38594ff
Natanael Mizzone <natanmizzone@gmail.com>
2026-03-27 22:30:26 -0300

    inserindo a documentação

--- pokemon_agenda/pokemon.cs
+++ pokemon_agenda/pokemon.cs
@@ -9,6 +9,7 @@ namespace pokemon_agenda
 {
     internal class Pokemon
     {
+
         string Nome;
         string Tipo;
         int Nivel;
@@ -16,11 +17,60 @@ namespace pokemon_agenda
         int Dano;
 
         //Criação da função de inicialização padrão do meu objeto
+
+
+        /// <summary>
+        /// Inicializa o construtor da minha classe padrão de Pokemon
+        /// <paramref name="nivelPokemon"/>, <paramref name="nomePokemon"/>, <paramref name="tipoPokemon"/>
+        /// 
+        /// <list type="bullet">
+        /// <item>nomePokemon: Variavel para saber o NOME</item>
+        /// <item>tipoPokemon: Variavel para saber o TIPO</item>
+        /// <item>nivelPokemon: Variavel para saber o NÍVEL</item>
+        /// </list>
+        /// </summary>
+        /// <returns>
+        /// A Class Pokemon retorna um objeto com todas as funções que tem relações.
+        /// </returns>
+        /// <param name="nomePokemon">Faz referência ao nome <c>Pokemoon</c></param>
+        /// <param name="tipoPokemon">Menciona a habilidade do <c>Pokemon</c></param>
+        /// <param name="nivelPokemon">Relaciona qual o nível da habilidade do <c>Pokemon</c></param>
+
+
+
+
+
+
+
+
+
+
+        /// <summary>
+        /// Inicializa o construtor da minha classe padrão de Pokemon
+        /// <list type="bullet">
+        /// <paramref name="nivelPokemon"/>, <paramref name="nomePokemon"/>, <paramref name="tipoPokemon"/>
+        /// <item>nomePokemon: Variavel para o Nome</item>
+        /// <item>tipoPokemon: Variavel para o Tipo</item>
+        /// <item>nivelPokemon: Variavel para o Nível</item>
+        /// </list>
+        /// <value>Aqui aparece a propriedade de valor</value>
+        /// </summary>
+        /// <param name="nomePokemon">Faz referência ao nome <c>Pokemoon</c></param>
+        /// <param name="tipoPokemon">Menciona a habilidade do <c>Pokemon</c></param>
+        /// <param name="nivelPokemon">Relaciona qual o nível da habilidade do <c>Pokemon</c></param>
+
+
         public Pokemon(string nomePokemon, string tipoPokemon, int nivelPokemon)
         {
+
+              Random r = new Random();
+            r.Next();
+
+
             // Nome = "Pikachu";
             //Tipo = "Elétrico";
             //Nivel = 5;
+
             Nome = nomePokemon  ;
             Tipo = tipoPokemon;
             Nivel = nivelPokemon;
@@ -30,13 +80,14 @@ namespace pokemon_agenda
 
         // public posso usar da função em qualquer espaço do meu projeto
         // private posso usar a função somente no meu arquivo original
-
+        /// <returns>
+        /// A função <see cref="fnDescricao"/> fnDescrição() não retorna nada.
+        /// </returns>
         public void fnDescricao()
         {
             MessageBox.Show($"pokemon: {Nome}, Tipo: {Tipo}, " +
                 $"Nivel: {Nivel}, Vida: {Vida}, Dano: {Dano}");
         }
-
         // nome do pokemon
         // tipo do pokemon
         // nivel do pokemon
